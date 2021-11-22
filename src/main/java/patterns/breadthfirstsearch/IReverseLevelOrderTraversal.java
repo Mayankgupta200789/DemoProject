fcaa15565609c6fbf9f30ded5cf85bf49ed36618
mayankgupta200789@gmail.com <mayankgupta200789@gmail.com>
2021-11-22 00:22:51 -0800

    Another good question. Although it looks like we need to use stack here but there is no need to use stack, we can simply solve this problem with adding the output list into first position of the result.

---
+++ src/main/java/patterns/breadthfirstsearch/IReverseLevelOrderTraversal.java
@@ -0,0 +1,51 @@
+package patterns.breadthfirstsearch;
+
+import tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+
+//https://leetcode.com/problems/binary-tree-level-order-traversal-ii/submissions/
+// Time complexity - O(n)
+// Space complexity - O(n)
+
+// Trick was to store always at 0th index
+public class IReverseLevelOrderTraversal {
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+
+
+        List<List<Integer>> result = new LinkedList<List<Integer>>();
+
+        if( root == null ) return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        List<Integer> output = new LinkedList<>();
+        queue.add(root);
+
+        while(!queue.isEmpty()) {
+
+            int levelSize = queue.size();
+
+            for(int i = 0; i < levelSize; i++ ) {
+
+                TreeNode node = queue.remove();
+                if(node != null ) output.add(node.val);
+                if(node != null && node.left != null ) queue.add(node.left);
+                if(node != null && node.right != null ) queue.add(node.right);
+            }
+
+            //*************************************************
+            //THIS IS REALLY IMPORTANT. THERE IS NO NEED TO USE STACK HERE
+            result.add(0,new ArrayList<>(output));
+            //*************************************************
+            output.clear();
+        }
+
+        return result;
+
+    }
+}
