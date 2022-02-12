b7500bb0795dc74c6f0962d20a2c181d7379acb8
mayagupt <ShahidThree@3>
2022-02-12 09:44:34 -0800

    Leet code submitted successfully.

---
+++ src/main/java/patterns/subsets/SubSets.java
@@ -0,0 +1,41 @@
+package patterns.subsets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Time complexity
+ * Every time inner loop goes for 2^n
+ * and this is called for N times
+ * thus, O(N*2^n)
+ *
+ * Space complexity
+ * Every time size becomes 2^n
+ * thus, space complexity becomes O(N*2^N)
+ *
+ *
+ */
+
+public class SubSets {
+
+    public List<List<Integer>> findSubSets(int[] nums) {
+
+        List<List<Integer>> subsets = new ArrayList<>();
+
+        subsets.add(new ArrayList<>());
+
+        for(int number : nums ) {
+            int n = subsets.size();
+            for(int i = 0 ; i < n ; i++ ) {
+
+                List<Integer> set = new ArrayList<>(subsets.get(i));
+                set.add(number);
+                subsets.add(set);
+            }
+        }
+
+        return subsets;
+    }
+
+
+}
