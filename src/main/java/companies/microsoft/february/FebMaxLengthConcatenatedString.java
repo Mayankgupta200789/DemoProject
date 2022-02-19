7b2d1dc9196cfce5fb215df9ed1784a611314028
mayagupt <ShahidThree@3>
2022-02-19 11:22:54 -0800

    For reference

--- src/main/java/companies/microsoft/february/FebMaxLengthConcatenatedString.java
+++ src/main/java/companies/microsoft/february/FebMaxLengthConcatenatedString.java
@@ -5,81 +5,33 @@ import java.util.*;
 public class FebMaxLengthConcatenatedString {
 
     public int maxLength(List<String> arr) {
-
-        // r act ers cha
-
-
-        int result = 0;
-        if(arr == null || arr.size() <= 1 ){
-            return result;
-        }
-
-        arr.sort((a, b) -> (b.length() - a.length()));
-
-        Set<String> uniqueSet = new LinkedHashSet<>(arr);
-
-
-
-        return minLen + maxLength;
-
-
-
-
+        return helper(arr,0,"");
     }
 
-    public int permuteUnique(List<String> arr) {
-
-        int result = 0;
-        if( arr == null || arr.size() == 0 ) {
-            return result;
+    private int helper(List<String> arr,int index,String cur){
+        if(index==arr.size()){
+            return cur.length();
         }
 
-        List<Integer> choices = new ArrayList<>();
-        arr.sort((a, b) -> (b.length() - a.length()));
-        Set<Character> hashSet = new HashSet<>();
-
-        for(int i = 0 ; i <  arr.size();i++ ) {
-
-            String input = arr.get(i);
-
-            int count =0 ;
-
-           for(int i =0 ; i < input.length(); i++ ) {
-
-
-               boolean flag = hashSet.add(input.charAt(i));
-               if()
-           }
-        }
-
-        doPermute(new ArrayList<>(),choices);
-
-        return result;
-
-    }
-
-    private void doPermute(List<Integer> prefix, List<Integer> choices) {
-
-        if( choices.size() == 0 ) {
-            List<Integer> temp = new ArrayList<>();
-            temp.addAll(prefix);
-            if( !hashSet.contains(temp)) {
-                hashSet.add(temp);
-                result.add(temp);
+        int res=cur.length();
+        for(int i=index;i<arr.size();i++){
+            String str=cur+arr.get(i);
+            if(isUnique(str)){
+                res=Math.max(res,helper(arr,i+1,str));
             }
-        } else {
 
-            for(int i = 0; i < choices.size(); i++ ) {
-                prefix.add(choices.get(i));
-                int element = choices.remove(i);
-                doPermute(prefix,choices);
-                prefix.remove(prefix.size() - 1);
-                choices.add(i,element);
+        }
+        return res;
+    }
+
+    private boolean isUnique(String s){
+        Set<Character> set=new HashSet<>();
+        for(char c:s.toCharArray()){
+            if(!set.add(c)){
+                return false;
             }
         }
-
-
+        return true;
     }
-
 
 }
